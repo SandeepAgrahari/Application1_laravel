96e02843478560be8a984940a1ed2bdf2399f1b7
Sandeep Kumar <sagrahari399@gmail.com>
2020-02-13 23:17:52 +0530

    Web Mix and all css, js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,24 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles([
+   'resources/css/libs/blog-post.css',
+   'resources/css/libs/bootstrap.css',
+   'resources/css/libs/bootstrap.min.css',
+   'resources/css/libs/font-awesome.css',
+   'resources/css/libs/metisMenu.css',
+   'resources/css/libs/sb-admin-2.css',
+   'resources/css/libs/styles.css',
+
+], 'public/css/libs.css');
+
+mix.scripts([
+   'resources/css/libs/bootstrap.js',
+   'resources/css/libs/bootstrap.min.js',
+   'resources/css/libs/jquery.js',
+   'resources/css/libs/metisMenu.js',
+   'resources/css/libs/sb-admin-2.js',
+   'resources/css/libs/scripts.js',
+
+], 'public/js/libs.js')
