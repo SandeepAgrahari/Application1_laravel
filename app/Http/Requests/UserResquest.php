201ac65f8f36248e519e43e06ef03d518ca50ad5
Sandeep Kumar <sagrahari399@gmail.com>
2020-03-09 00:17:20 +0530

    Add user without photo done

--- app/Http/Requests/UserResquest.php
+++ app/Http/Requests/UserResquest.php
@@ -24,9 +24,30 @@ class UserResquest extends FormRequest
     public function rules()
     {
         return [
-            'name'=>'required',
-            'email'=>'required',
-            'password'=>'required'
+            'name' => 'required',
+            'email' => 'required',
+            'role_id' => 'required',
+            'status' => 'required',
+            'password' => 'required',
+            'cpassword' => 'required'
         ];
+    }
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name can not be blank',
+            'email.required' => 'email can not be blank',
+            'role_id.required' => 'Select any one of role',
+            'status.required' => 'Select any one of Status',
+            'password.required' => 'Password can not be blank',
+            'cpassword.required' => 'confirm password can not be blank'
+            // 'name.required' => 'Name can not be blank',
+        ];
+
     }
 }
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -7,41 +7,62 @@
             <h3 class="block-title">Create User</h3>
     </div> 
     <div class="block-content">
-        {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
+        {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store', 'files'=>true]) !!}
             <div class="row">
-                <div class="form-group col col-sm-6">
+                <div class="form-group col col-sm-6 {{ $errors->has('name') ? 'has-error is-invalid' : ''}}">
                     <div class="form-material">
                             {!! Form::text('name', null, ['class' =>'form-control', 'placeholder'=> 'Enter a name..', 'id'=>'name']) !!}
+                            {!! $errors->first('name', '<p class="help-block invalid-feedback">:message</p>') !!}
                             {!! Html::decode(Form::label('name', 'Name <span class="text-danger">*</span>', ['class'=>'col-form-label'])) !!}
                     </div>
                 </div>
-                <div class="form-group col col-sm-6">
+                <div class="form-group col col-sm-6 {{ $errors->has('email') ? 'has-error is-invalid' : ''}}">
                     <div class="form-material">
                             {!! Form::email('email', null, ['class' =>'form-control', 'placeholder'=> 'Enter a email..', 'id'=>'email']) !!}
+                            {!! $errors->first('email', '<p class="help-block invalid-feedback">:message</p>') !!}
                             {!! Html::decode(Form::label('email', 'Email <span class="text-danger">*</span>', ['class'=>'col-form-label'])) !!}
                     </div>
                 </div>
             </div>
             <div class="row">
-                    <div class="form-group col col-sm-6">
+                    <div class="form-group col col-sm-6 {{ $errors->has('role_id') ? 'has-error is-invalid' : ''}}">
                             <div class="form-material">
                                 {!! Form::select('role_id', [''=>'Select Role'] + $roles, null , ['class'=>'form-control', 'id'=>'role_id']) !!}
+                                {!! $errors->first('role_id', '<p class="help-block invalid-feedback">:message</p>') !!}
                                 {!! Html::decode(Form::label('role_id', 'Role <span class="text-danger">*</span>', ['class'=>'col-form-label'])) !!}
                             </div>
                     </div>
-                    <div class="form-group col col-sm-6">
+                    <div class="form-group col col-sm-6 {{ $errors->has('status') ? 'has-error is-invalid' : ''}}">
                             <div class="form-material">
                                 {!! Form::select('status', ['1'=>'Active','0'=>'Not Active'], 0 , ['class'=>'form-control', 'id'=>'status']) !!}
+                                {!! $errors->first('status', '<p class="help-block invalid-feedback">:message</p>') !!}
                                 {!! Html::decode(Form::label('status', 'Status <span class="text-danger">*</span>', ['class'=>'col-form-label'])) !!}
                             </div>
                     </div>
             </div>
             <div class="row">
-                <div class="form-group col col-sm-6">
+                <div class="form-group col col-sm-6 {{ $errors->has('password') ? 'has-error is-invalid' : ''}}">
                         <div class="form-material">
                             {!! Form::password('password', ['class'=>'form-control', 'placeholder'=> 'Enter a password..', 'id'=>'password']) !!}
+                            {!! $errors->first('password', '<p class="help-block invalid-feedback">:message</p>') !!}
                             {!! Html::decode(Form::label('password', 'Password <span class="text-danger">*</span>', ['class'=>'col-form-label'])) !!}
                         </div>
+                </div>
+                <div class="form-group col col-sm-6 {{ $errors->has('cpassword') ? 'has-error is-invalid' : ''}}">
+                    <div class="form-material">
+                        {!! Form::password('cpassword', ['class'=>'form-control', 'placeholder'=> 'Enter a confirm password..', 'id'=>'cpassword']) !!}
+                        {!! $errors->first('cpassword', '<p class="help-block invalid-feedback">:message</p>') !!}
+                        {!! Html::decode(Form::label('cpassword', 'Confirm Password <span class="text-danger">*</span>', ['class'=>'col-form-label'])) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group col col-sm-6 {{ $errors->has('photo') ? 'has-error is-invalid' : ''}}">
+                    <div class="form-material">
+                        {!! Form::file('photo', ['class'=>'form-control remove-box-shadow', 'id'=>'photo']) !!}
+                        {!! $errors->first('photo', '<p class="help-block invalid-feedback">:message</p>') !!}
+                        {!! Html::decode(Form::label('photo', 'Profile Picture <span class="text-danger">*</span>', ['class'=>'col-form-label'])) !!}
+                    </div>
                 </div>
             </div>
             <div class="row">
@@ -52,5 +73,17 @@
         {!! Form::close() !!}
     </div> 
 </div>       
-    
+
+{{-- @if(count($errors) >0)
+
+        <div class="alert alert-danger">
+
+            <ul>
+                @foreach ($errors as $error)
+                    <li>{{$error->message}}</li>
+                @endforeach
+            <ul>
+        </div>
+
+@endif --}}
 @endsection
