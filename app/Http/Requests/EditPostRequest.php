08ce00352b5a80fb108884cc38dfaa6f36d948a0
Sandeep Kumar <sagrahari399@gmail.com>
2020-03-28 20:11:04 +0530

    Edit Functionality for post => Done

---
+++ app/Http/Requests/EditPostRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'=>'required',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+    }
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'title.required' => 'Title can not be blank',
+            'category_id.required' => 'Select any one of the category',
+            'body.required' => 'Description can not be blank'
+        ];
+
+    }
+}
